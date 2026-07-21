13b13d63b9e615de3c086c82a941318ab48ab29e
aman <amanm.ug23.cs@nitp.ac.in>
2026-07-21 17:13:12 +0530

    Addded button

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -3,10 +3,10 @@
 export default function SubmitButton() {
   return (
     <button 
-      onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLScDNdLRnB6OmX913KqqjYiw9eQI5Thk9h0MaIFOruGUHTtL5Q/viewform')}
+      onClick={() => window.open('https://share.google/06qMnzK0XJM5XvQHz')}
       className="btn submit-paper"
     >
-      Register
+      Submit Paper
     </button>
   );
 }
